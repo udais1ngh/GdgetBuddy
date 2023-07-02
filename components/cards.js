8531f54434c9b36b3662b74cb821a866900d665a
udais1ngh <udaisingh3520@gmail.com>
2023-07-02 13:13:27 +0530

    tostify duration decreased

--- components/cards.js
+++ components/cards.js
@@ -62,7 +62,7 @@ const notify = () => {
 
   toast.success("Added To Cart!", {
     position: "top-right",
-autoClose: 2000,
+autoClose: 1000,
 hideProgressBar: false,
 closeOnClick: true,
 pauseOnHover: true,
